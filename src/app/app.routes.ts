1296a7a6985303a27807428abfdf2f6c0c00782a
felipe <felipe.alves2014@gmail.com>
2025-05-11 13:17:35 -0300

    sobre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainPageComponent},
+    {path: 'about', component: AboutComponent}
+];
