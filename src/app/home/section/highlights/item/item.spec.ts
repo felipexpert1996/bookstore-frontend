6870fa4c046240ba762359cbe1257e1daedb1a9f
felipe <felipe.alves2014@gmail.com>
2025-10-04 21:45:11 -0300

    unit tests

--- src/app/home/section/highlights/item/item.spec.ts
+++ src/app/home/section/highlights/item/item.spec.ts
@@ -31,4 +31,40 @@ describe('Item', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render book title', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#title')?.textContent).toContain('book Test');
+  });
+
+  it('should render book author', () => {
+    component.item = { id: '1', title: 'book Test', author: 'author Test', description: 'test description', imageUrl: 'https://m.media-amazon.com/images/I/81tmn7qB-bL._UF1000,1000_QL80_.jpg', price: 9.99};
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#author')?.textContent).toContain('author Test');
+  });
+
+  it('should render book price', () => {
+    component.item = { id: '1', title: 'book Test', author: 'author Test', description: 'test description', imageUrl: 'https://m.media-amazon.com/images/I/81tmn7qB-bL._UF1000,1000_QL80_.jpg', price: 9.99};
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const text = compiled.querySelector('#price')?.textContent;
+    const normalizedText = text?.replace(/\s/g, ' ').trim();
+    expect(normalizedText).toBe('R$ 9,99');
+  });
+
+  it('should render book image', () => {
+    component.item = { id: '1', title: 'book Test', author: 'author Test', description: 'test description', imageUrl: 'https://m.media-amazon.com/images/I/81tmn7qB-bL._UF1000,1000_QL80_.jpg', price: 9.99};
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('img')?.getAttribute('src')).toBe('https://m.media-amazon.com/images/I/81tmn7qB-bL._UF1000,1000_QL80_.jpg');
+  });
+
+  it('should call addToCart method when button is clicked', () => {
+    spyOn(component, 'addToCart');
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button');
+    button?.click();
+    expect(component.addToCart).toHaveBeenCalledWith(component.item);
+  });
 });
--- src/app/services/cart/cart.spec.ts
+++ src/app/services/cart/cart.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CartService } from './cart';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('Cart', () => {
   let service: CartService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()]
+    });
     service = TestBed.inject(CartService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be created with default values', () => {
+    expect(service.getTotalItems()).toEqual(0);
+    expect(service.getTotalPrice()).toEqual(0);
+  });
+
+  it('should add item to cart', () => {
+    service.addToCart({ id: '1', name: 'Book 1', price: 10, quantity: 1 });
+    expect(service.getTotalItems()).toEqual(1);
+    expect(service.getTotalPrice()).toEqual(10);
+  });
+
+  it('should remove item from cart', () => {
+    service.addToCart({ id: '1', name: 'Book 1', price: 10, quantity: 1 });
+    service.removeFromCart('1');
+    expect(service.getTotalItems()).toEqual(0);
+    expect(service.getTotalPrice()).toEqual(0);
+  });
+
+  it('should clear cart', () => {
+    service.addToCart({ id: '1', name: 'Book 1', price: 10, quantity: 1 });
+    service.addToCart({ id: '2', name: 'Book 2', price: 20, quantity: 2 });
+    service.clearCart();
+    expect(service.getTotalItems()).toEqual(0);
+    expect(service.getTotalPrice()).toEqual(0);
+  });
+
+  it('should update quantity if item already exists in cart', () => {
+    service.addToCart({ id: '1', name: 'Book 1', price: 10, quantity: 1 });
+    service.addToCart({ id: '1', name: 'Book 1', price: 10, quantity: 2 });
+    expect(service.getTotalItems()).toEqual(3);
+    expect(service.getTotalPrice()).toEqual(30);
   });
 });
